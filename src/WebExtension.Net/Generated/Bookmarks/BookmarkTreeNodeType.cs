7b4bb3491fc638a3094191c433dd86cfe9813775
newbe36524 <newbe36524@qq.com>
2021-04-29 09:31:39 +0800

    Add Bookmarks

---
+++ src/WebExtension.Net/Generated/Bookmarks/BookmarkTreeNodeType.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace WebExtension.Net.Bookmarks
+{
+    /// <summary>Indicates the type of a BookmarkTreeNode, which can be one of bookmark, folder or separator.</summary>
+    [JsonConverter(typeof(EnumStringConverter<BookmarkTreeNodeType>))]
+    public enum BookmarkTreeNodeType
+    {
+        /// <summary>bookmark</summary>
+        [EnumValue("bookmark")]
+        Bookmark,
+
+        /// <summary>folder</summary>
+        [EnumValue("folder")]
+        Folder,
+
+        /// <summary>separator</summary>
+        [EnumValue("separator")]
+        Separator,
+    }
+}
---
+++ src/WebExtension.Net/Generated/Bookmarks/CreateDetails.cs
@@ -0,0 +1,90 @@
+using System.Text.Json.Serialization;
+
+namespace WebExtension.Net.Bookmarks
+{
+    // Type Class
+    /// <summary>Object passed to the create() function.</summary>
+    public class CreateDetails : BaseObject
+    {
+        private int? _index;
+        private string _parentId;
+        private string _title;
+        private BookmarkTreeNodeType _type;
+        private string _url;
+
+        /// <summary></summary>
+        [JsonPropertyName("index")]
+        public int? Index
+        {
+            get
+            {
+                InitializeProperty("index", _index);
+                return _index;
+            }
+            set
+            {
+                _index = value;
+            }
+        }
+
+        /// <summary>Defaults to the Other Bookmarks folder.</summary>
+        [JsonPropertyName("parentId")]
+        public string ParentId
+        {
+            get
+            {
+                InitializeProperty("parentId", _parentId);
+                return _parentId;
+            }
+            set
+            {
+                _parentId = value;
+            }
+        }
+
+        /// <summary></summary>
+        [JsonPropertyName("title")]
+        public string Title
+        {
+            get
+            {
+                InitializeProperty("title", _title);
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+
+        /// <summary>Indicates the type of BookmarkTreeNode to create, which can be one of bookmark, folder or separator.</summary>
+        [JsonPropertyName("type")]
+        public BookmarkTreeNodeType Type
+        {
+            get
+            {
+                InitializeProperty("type", _type);
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        /// <summary></summary>
+        [JsonPropertyName("url")]
+        public string Url
+        {
+            get
+            {
+                InitializeProperty("url", _url);
+                return _url;
+            }
+            set
+            {
+                _url = value;
+            }
+        }
+    }
+}
