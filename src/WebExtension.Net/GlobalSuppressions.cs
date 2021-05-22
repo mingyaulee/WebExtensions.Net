c6f8f7195ee56548718b2431999ed470da3a4e63
Ming Yau Lee <lee_mingyau@hotmail.com>
2021-05-22 09:39:04 +0800

    Add global suppression attributes

---
+++ src/WebExtension.Net/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Maintainability", "CA1507:Use nameof to express symbol names", Justification = "Code is generated", Scope = "member", Target = "~P:WebExtension.Net.ExtensionTypes.Rect.X")]
+[assembly: SuppressMessage("Maintainability", "CA1507:Use nameof to express symbol names", Justification = "Code is generated", Scope = "member", Target = "~P:WebExtension.Net.ExtensionTypes.Rect.Y")]
