d9b9af37d4f8416cfa084a3da5faea3d7559bfcf
Ming Yau Lee <lee_mingyau@hotmail.com>
2021-07-05 15:10:32 +0800

    Extend ExtensionTypes.Date to have implicit conversion from System.DateTime

---
+++ src/WebExtensions.Net/Extensions/ExtensionTypes/Date.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebExtensions.Net.ExtensionTypes
+{
+    public partial class Date
+    {
+        /// <summary>Converts from <see cref="DateTime" /> to <see cref="Date" />.</summary>
+        /// <param name="value">The value to convert from.</param>
+        public static implicit operator Date(DateTime value) => new((EpochTime)value);
+    }
+}
