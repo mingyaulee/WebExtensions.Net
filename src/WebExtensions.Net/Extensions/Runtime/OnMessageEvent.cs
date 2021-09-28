931568da98d2042c507d8d461df64a77a4bf7570
Z4t4r <771626206@qq.com>
2021-09-28 11:41:36 +0800

    move to extensions

--- src/WebExtensions.Net/Extensions/Runtime/OnMessageEvent.cs
+++ src/WebExtensions.Net/Extensions/Runtime/OnMessageEvent.cs
@@ -1,33 +1,31 @@
-﻿using JsBind.Net;
-using System;
+﻿using System;
 using System.Threading.Tasks;
-using WebExtensions.Net.Events;
+using WebExtensions.Net.Runtime;
+using JsBind.Net;
 
 namespace WebExtensions.Net.Runtime
 {
     // Type Class
     /// <summary>Fired when a message is sent from either an extension process or a content script.</summary>
-    [BindAllProperties]
-    public partial class OnMessageEvent : Event
+    /// 
+    public partial class OnMessageEvent
     {
         /// <summary>Registers an event listener <em>callback</em> to an event.</summary>
         /// <param name="callback">Fired when a message is sent from either an extension process or a content script.</param>
-        public virtual ValueTask AddListener(Func<object, MessageSender, Action, bool> callback)
+        public virtual ValueTask AddListener(Func<object, MessageSender, Action<object>, bool> callback)
         {
             return InvokeVoidAsync("addListener", callback);
         }
-
         /// <summary></summary>
         /// <param name="callback">Listener whose registration status shall be tested.</param>
         /// <returns>True if <em>callback</em> is registered to the event.</returns>
-        public virtual ValueTask<bool> HasListener(Func<object, MessageSender, Action, bool> callback)
+        public virtual ValueTask<bool> HasListener(Func<object, MessageSender, Action<object>, bool> callback)
         {
             return InvokeAsync<bool>("hasListener", callback);
         }
-
         /// <summary>Deregisters an event listener <em>callback</em> from an event.</summary>
         /// <param name="callback">Listener that shall be unregistered.</param>
-        public virtual ValueTask RemoveListener(Func<object, MessageSender, Action, bool> callback)
+        public virtual ValueTask RemoveListener(Func<object, MessageSender, Action<object>, bool> callback)
         {
             return InvokeVoidAsync("removeListener", callback);
         }
