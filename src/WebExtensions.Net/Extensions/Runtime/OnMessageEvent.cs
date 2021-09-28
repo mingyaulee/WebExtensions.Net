a2d86d2f4c308251f58bdc8a69d2627109b21439
Z4t4r <771626206@qq.com>
2021-09-27 23:54:44 -0500

    Add overload for listener in OnMessageEvent (#11)

---
+++ src/WebExtensions.Net/Extensions/Runtime/OnMessageEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebExtensions.Net.Runtime;
+using JsBind.Net;
+
+namespace WebExtensions.Net.Runtime
+{
+    public partial class OnMessageEvent
+    {
+        /// <summary>Registers an event listener <em>callback</em> to an event.</summary>
+        /// <param name="callback">Fired when a message is sent from either an extension process or a content script.</param>
+        public virtual ValueTask AddListener(Func<object, MessageSender, Action<object>, bool> callback)
+        {
+            return InvokeVoidAsync("addListener", callback);
+        }
+
+        /// <summary></summary>
+        /// <param name="callback">Listener whose registration status shall be tested.</param>
+        /// <returns>True if <em>callback</em> is registered to the event.</returns>
+        public virtual ValueTask<bool> HasListener(Func<object, MessageSender, Action<object>, bool> callback)
+        {
+            return InvokeAsync<bool>("hasListener", callback);
+        }
+
+        /// <summary>Deregisters an event listener <em>callback</em> from an event.</summary>
+        /// <param name="callback">Listener that shall be unregistered.</param>
+        public virtual ValueTask RemoveListener(Func<object, MessageSender, Action<object>, bool> callback)
+        {
+            return InvokeVoidAsync("removeListener", callback);
+        }
+    }
+}
