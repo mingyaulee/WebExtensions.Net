a50f4e6982f77369e983f5c72e8dca0e78d9b840
Ming Yau Lee <lee_mingyau@hotmail.com>
2021-10-07 18:39:50 +0800

    Add overload for Event methods to accept Delegate type

---
+++ src/WebExtensions.Net/Extensions/Events/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebExtensions.Net.Events
+{
+    public partial class Event
+    {
+        /// <summary>Registers an event listener <em>callback</em> to an event.</summary>
+        /// <param name="callback">Called when an event occurs. The parameters of this function depend on the type of event.</param>
+        public virtual ValueTask AddListener(Delegate callback)
+        {
+            return InvokeVoidAsync("addListener", callback);
+        }
+
+        /// <summary></summary>
+        /// <param name="callback">Listener whose registration status shall be tested.</param>
+        /// <returns>True if <em>callback</em> is registered to the event.</returns>
+        public virtual ValueTask<bool> HasListener(Delegate callback)
+        {
+            return InvokeAsync<bool>("hasListener", callback);
+        }
+
+        /// <summary>Deregisters an event listener <em>callback</em> from an event.</summary>
+        /// <param name="callback">Listener that shall be unregistered.</param>
+        public virtual ValueTask RemoveListener(Delegate callback)
+        {
+            return InvokeVoidAsync("removeListener", callback);
+        }
+    }
+}
